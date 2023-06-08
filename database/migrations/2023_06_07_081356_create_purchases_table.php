a8cd704f903a256bb928eea18de95014be635163
Alfito24 <78544217+Alfito24@users.noreply.github.com>
2023-06-08 07:56:25 +0700

    fix-library

--- database/migrations/2023_06_07_081356_create_purchases_table.php
+++ database/migrations/2023_06_07_081356_create_purchases_table.php
@@ -11,10 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('purchases', function (Blueprint $table) {
-            $table->id();
+        Schema::create('purchases_store', function (Blueprint $table) {
+            $table->id('purchase_id')->primary();
+            $table->integer('sepatu')->nullable();
+            $table->integer('sandal')->nullable();
+            $table->integer('snack')->nullable();
+            $table->integer('soda_kaleng')->nullable();
+            $table->integer('tissue')->nullable();
+            $table->integer('minyak_goreng')->nullable();
+            $table->integer('gula')->nullable();
+            $table->integer('pasta_gigi')->nullable();
+            $table->integer('detergen')->nullable();
+            $table->integer('lampu')->nullable();
+            $table->foreignUuid('deterjen')->constraint();
             $table->timestamps();
         });
+        // sepatu, sandal, snack, soda kaleng, tisue, minyak goreng, gula, pasta gigi, sabun cuci piring, deterjen
     }
 
     /**
